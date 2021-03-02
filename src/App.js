de187f0a904b5339cec2d10f370adc0be602e650
Arthur Abreu Damiani <arthur.damiani@gmail.com>
2021-03-02 16:16:58 -0300

    feat: guthub corner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Routes from './routes'
+import GitHubCorner from './components/GitHubCorner'
 
 function App() {
   return (
     <>
+      <GitHubCorner projectUrl='https://github.com/arthurDamiani/githubExplorer' />
       <Routes />
     </>
   );
